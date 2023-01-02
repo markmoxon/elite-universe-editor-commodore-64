5e08a19521def0896eec35dbdac52270b77099fc
Mark Moxon <mark@moxon.net>
2023-01-02 20:14:12 +0000

    Add file access variables for save and load in Commodore 64 version

--- src/elite-universe-editor-c64.asm
+++ src/elite-universe-editor-c64.asm
@@ -27,180 +27,204 @@ _C64_VERSION    = TRUE
 \
 \ ******************************************************************************
 
-NI%         = 37
-NOSH        = 10
-NTY         = 33
-MSL         = 1
-SST         = 2
+NI%             = 37
+NOSH            = 10
+NTY             = 33
+MSL             = 1
+SST             = 2
 
-VE          = $57
+VE              = $57
 
-YELLOW2     = %10101010
-GREEN2      = %11111111
+YELLOW2         = %10101010
+GREEN2          = %11111111
 
-T1          = $0006
-SC          = $0007
-SCH         = $0008
-INWK        = $0009
-NEWB        = $002D
-P           = $002E
-XC          = $0031
-YC          = $0033
-QQ17        = $0034
-K3          = $0035
-XX0         = $0057
-INF         = $0059
-V           = $005B
-BETA        = $0063
-BET1        = $0064
-ECMA        = $0067
-ALP1        = $0068
-ALP2        = $0069
-K           = $0077
-KL          = $007D
-QQ15        = $007F
-DELTA       = $0096
-A           = $009A
-U           = $0099
-Q           = $009A
-R           = $009B
-S           = $009C
-QQ11        = $00A0
-MCNT        = $00A3
-TYPE        = $00A5
-QQ12        = $00A7
-RAT2        = $00B1
-T           = $00BB
+CPU_PORT        = $0001
 
-FRIN        = $0452
-MANY        = $045D
-JUNK        = $047F
-auto        = $0480     \ LDX #(de-auto) in PlayUniverse
-CABTMP      = $0483
-VIEW        = $0486
-GNTMP       = $0488
-DLY         = $048B
-de          = $048C     \ LDX #(de-auto) in PlayUniverse
-JSTX        = $048D
-JSTY        = $048E
-NAME        = $0491
-QQ0         = $049A
-QQ1         = $049B
-QQ21        = $049C
-ESCP        = $04C7
-NOMSL       = $04CC
-FSH         = $04E7
-ASH         = $04E8
-ENERGY      = $04E9
-QQ28        = $04EE
-gov         = $04F0
-tek         = $04F1
-SLSP        = $04F2
-QQ2         = $04F4
-QQ14        = $04A6
-CHK2        = $04E3
-CHK         = $04E4
-QQ25        = $04ED
-QQ3         = $0500
-QQ4         = $0501
-QQ5         = $0502
-ALTIT       = $06F3
+CIA1_ICSREG     = $DC0D
+VIC_SCREG1      = $D011
+VIC_RASTER      = $D012
+VIC_ICREG       = $D01A
 
-DETOK       = $2390
-DTEN        = $23A0
-MT19        = $24ED
-S1%         = $25A6
-NA%         = $25AB
-UNIV        = $28A4
-NLIN4       = $28DC
-MVT3        = $2D69
-MVS5        = $2DC5
-pr2         = $2E55
-BEEP        = $2FEE
-DIALS       = $2FF3
-PZW2        = $30BB     \ STA PZW2-3 in ApplyMods/RevertMods
-GINF        = $3E87
-ping        = $3E95
-DELAY       = $3EA1
-DOXC        = $6A25
-DOYC        = $6A28
-DOVDU19     = $6A2E
-TRADEMODE   = $6A2F
-TT67        = $6A8E
-BAY2        = $6DBF
-gnum        = $6DC9
-TT111       = $70AB
-G1          = $71E8     \ JSR G1-13 in DrawCharts
-zZ          = $71F2
-jmp         = $7217
-ee3         = $7224
-pr6         = $7234
-prq         = $723C
-TT162       = $72C5
-TT114       = $7452
-DOEXP       = $7866     \ STA DOEXP in ApplyExplosionMod/RevertExplosionMod
-PTCLS       = $78D6
-SOLAR       = $7AC2
-NWSTARS     = $7AF3
-WPSHPS      = $7B1A
-WS1         = $7B41     \ STA WS1-3 in ApplyMods/RevertMods
-SP2         = $7BAB
-NWSPS       = $7C24     \ JSR NWSPS+3 in SwapStationSun
-NWSHP       = $7C6B
-NwS1        = $7D03
-WPLS        = $80FF
-GETYN       = $81EE
-TT17        = $81FB
-KS4         = $82A4
-KILLSHP     = $82F3
-RESET       = $83CA
-RES2        = $83DF
-yu          = $8437     \ STA yu+3 in PlayUniverse
-ZINF        = $8447
-msblob      = $845C
-SAL8        = $846C     \ STY SAL8+1 in UpdateDashboard
-TT100       = $84ED
-TT102       = $86B1     \ JSR TT102+7 in DrawCharts
-TT92        = $86D0     \ STA TT92-7 in ApplyMods/RevertMods
-NWDAV5      = $872C     \ LDA #NWDAV5-TT92+6 in ApplyMods
-BR1         = $8882
-DFAULT      = $88F0
-TITLE       = $8920
-CHECK       = $89EB
-GTL2        = $8A2F     \ STA GTL2+1 in ShowDiscMenu/RevertDiscMods
-GTNMEW      = $8A38
-MT26        = $8A5B
-U%          = $8C6D
-TAS2        = $8C8A
-RDKEY       = $8D53
-CTRL        = $8E92
-TT217       = $8FEA
-t           = $8FEC
-TIDY        = $9105
-LL9         = $9A86
-LL74        = $9F35     \ STA LL74+16 in ApplyMods/RevertMods
-MV5         = $A434
-PLUT        = $A626
-LOOK1       = $A6BA
-SIGHT       = $A6D4
-TT66        = $A72F
-NOISE       = $A858
-DOT         = $B09D
-ECBLB       = $B0FD
-SPBLB       = $B10E
-SCAN        = $B410
-XX21        = $D000
-K%          = $F900
+SETMSG          = $FF90
+READST          = $FFB7
+OPEN            = $FFC0
+CLOSE           = $FFC3
+CHKIN           = $FFC6
+CHKOUT          = $FFC9
+CLRCHN          = $FFCC
+CHRIN           = $FFCF
+CHROUT          = $FFD2
 
-keyLog      = $8D0C     \ Key logger table, which is distinct from KL
+                        \ Variables from the Commodore 64 version that aren't in
+                        \ the BBC versions
 
-GTDRV = 0   \ Not used in C64? Used in SaveLoadFile in UE
-CATS = 0    \ Not used in C64? Used in DeleteUniverse and ReturnToDiscMenu in UE
-stack = 0   \ Not used in C64? Used in ShowDiscMenu in UE
+keyLog          = $8D0C \ Key logger table, which is distinct from KL
+screenSection   = $A8D9 \ New variable for Commodore 64 version
+
+                        \ Variables in both the BBC versions and the Commodore
+                        \ 64 version
+
+T1              = $0006
+SC              = $0007
+SCH             = $0008
+INWK            = $0009
+NEWB            = $002D
+P               = $002E
+XC              = $0031
+YC              = $0033
+QQ17            = $0034
+K3              = $0035
+XX0             = $0057
+INF             = $0059
+V               = $005B
+BETA            = $0063
+BET1            = $0064
+ECMA            = $0067
+ALP1            = $0068
+ALP2            = $0069
+K               = $0077
+KL              = $007D
+QQ15            = $007F
+DELTA           = $0096
+A               = $009A
+U               = $0099
+Q               = $009A
+R               = $009B
+S               = $009C
+QQ11            = $00A0
+MCNT            = $00A3
+TYPE            = $00A5
+QQ12            = $00A7
+RAT2            = $00B1
+T               = $00BB
+
+FRIN            = $0452
+MANY            = $045D
+JUNK            = $047F
+auto            = $0480 \ LDX #(de-auto) in PlayUniverse
+CABTMP          = $0483
+VIEW            = $0486
+GNTMP           = $0488
+DLY             = $048B
+de              = $048C \ LDX #(de-auto) in PlayUniverse
+JSTX            = $048D
+JSTY            = $048E
+NAME            = $0491
+QQ0             = $049A
+QQ1             = $049B
+QQ21            = $049C
+ESCP            = $04C7
+NOMSL           = $04CC
+FSH             = $04E7
+ASH             = $04E8
+ENERGY          = $04E9
+QQ28            = $04EE
+gov             = $04F0
+tek             = $04F1
+SLSP            = $04F2
+QQ2             = $04F4
+QQ14            = $04A6
+CHK2            = $04E3
+CHK             = $04E4
+QQ25            = $04ED
+QQ3             = $0500
+QQ4             = $0501
+QQ5             = $0502
+LSO             = $0580
+ALTIT           = $06F3
+
+DETOK           = $2390
+DTEN            = $23A0
+MT19            = $24ED
+S1%             = $25A6
+NA%             = $25AB
+UNIV            = $28A4
+NLIN4           = $28DC
+MVT3            = $2D69
+MVS5            = $2DC5
+pr2             = $2E55
+BEEP            = $2FEE
+DIALS           = $2FF3
+PZW2            = $30BB \ STA PZW2-3 in ApplyMods/RevertMods
+GINF            = $3E87
+ping            = $3E95
+DELAY           = $3EA1
+DOXC            = $6A25
+DOYC            = $6A28
+DOVDU19         = $6A2E
+TRADEMODE       = $6A2F
+TT67            = $6A8E
+BAY2            = $6DBF
+gnum            = $6DC9
+TT111           = $70AB
+G1              = $71E8 \ JSR G1-13 in DrawCharts
+zZ              = $71F2
+jmp             = $7217
+ee3             = $7224
+pr6             = $7234
+prq             = $723C
+TT162           = $72C5
+TT114           = $7452
+SWAPZP          = $784F
+DOEXP           = $7866 \ STA DOEXP in ApplyExplosionMod/RevertExplosionMod
+PTCLS           = $78D6
+SOLAR           = $7AC2
+NWSTARS         = $7AF3
+WPSHPS          = $7B1A
+WS1             = $7B41 \ STA WS1-3 in ApplyMods/RevertMods
+SP2             = $7BAB
+NWSPS           = $7C24 \ JSR NWSPS+3 in SwapStationSun
+NWSHP           = $7C6B
+NwS1            = $7D03
+WPLS            = $80FF
+GETYN           = $81EE
+TT17            = $81FB
+SetMemory       = $827F
+KS4             = $82A4
+KILLSHP         = $82F3
+RESET           = $83CA
+RES2            = $83DF
+yu              = $8437 \ STA yu+3 in PlayUniverse
+ZINF            = $8447
+msblob          = $845C
+SAL8            = $846C \ STY SAL8+1 in UpdateDashboard
+TT100           = $84ED
+TT102           = $86B1 \ JSR TT102+7 in DrawCharts
+TT92            = $86D0 \ STA TT92-7 in ApplyMods/RevertMods
+NWDAV5          = $872C \ LDA #NWDAV5-TT92+6 in ApplyMods
+BR1             = $8882
+DFAULT          = $88F0
+TITLE           = $8920
+CHECK           = $89EB
+GTL2            = $8A2F \ STA GTL2+1 in ShowDiscMenu/RevertDiscMods
+GTNMEW          = $8A38
+MT26            = $8A5B
+GTDRV           = $8BC0
+U%              = $8C6D
+TAS2            = $8C8A
+RDKEY           = $8D53
+CTRL            = $8E92
+TT217           = $8FEA
+t               = $8FEC
+TIDY            = $9105
+LL9             = $9A86
+LL74            = $9F35 \ STA LL74+16 in ApplyMods/RevertMods
+MV5             = $A434
+PLUT            = $A626
+LOOK1           = $A6BA
+SIGHT           = $A6D4
+TT66            = $A72F
+NOISE           = $A858
+DOT             = $B09D
+ECBLB           = $B0FD
+SPBLB           = $B10E
+SCAN            = $B410
+XX21            = $D000
+K%              = $F900
 
 \ ******************************************************************************
 \
-\ Save EDITOR.unprot.bin
+\ Save EDITOR.bin
 \
 \ ******************************************************************************
 
@@ -230,8 +254,8 @@ ORG CODE%
  CMP #f0                \ Did we press f1/f2? If not, skip the following
  BNE P%+5               \ instruction
 
- JMP UniverseEditor     \ We pressed f1/f2, so jump to UniverseEditor to start the
-                        \ universe editor
+ JMP UniverseEditor     \ We pressed f1/f2, so jump to UniverseEditor to start
+                        \ the universe editor
 
  RTS                    \ Return from the subroutine
 
@@ -304,8 +328,9 @@ ORG CODE%
  CPX #keyDown           \ If the down arrow is not being pressed, jump to scur2
  BNE scur2              \ to keep checking
 
-                        \ If we get here then the down arrow is being pressed, so
-                        \ we need to check whether SHIFT is also being pressed
+                        \ If we get here then the down arrow is being pressed,
+                        \ so we need to check whether SHIFT is also being
+                        \ pressed
 
  BIT keyLog+keyShiftL   \ If the left SHIFT is being pressed, jump to scur1 to
  BMI scur1              \ set KL to the up arrow (i.e. SHIFT-down arrow)
